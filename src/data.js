cd29e6ee1b39a82fde0733d0ef3873f22c934107
YAMINI-R-P <yaminirp2003@gmail.com>
2022-11-11 12:28:25 +0530

    asd

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const sliderItems=[
+    {
+      id:1,
+      img:"https://previews.123rf.com/images/subbotina/subbotina1601/subbotina160100032/50758762-fashion-model-girl-isolated-over-white-background.jpg",
+      title:"SUMMER SALE",
+      desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg:"white",
+},
+{
+      id:2,
+      img:"https://previews.123rf.com/images/subbotina/subbotina1601/subbotina160100032/50758762-fashion-model-girl-isolated-over-white-background.jpg",
+      title:"WINTER SALE",
+      desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg:"fcf1ed",
+
+},
+{  
+      id:3,
+      img:"https://previews.123rf.com/images/subbotina/subbotina1601/subbotina160100032/50758762-fashion-model-girl-isolated-over-white-background.jpg",
+      title:"FESTIVE SALE",
+      desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg:"fbf0f4",
+
+}];
+
+export const categories=[
+    {
+      id:1,
+      img:"https://previews.123rf.com/images/subbotina/subbotina1601/subbotina160100032/50758762-fashion-model-girl-isolated-over-white-background.jpg",
+      title:"SUMMER SALE",
+},
+{
+      id:2,
+      img:"https://previews.123rf.com/images/subbotina/subbotina1601/subbotina160100032/50758762-fashion-model-girl-isolated-over-white-background.jpg",
+      title:"WINTER SALE",
+
+},
+{  
+      id:3,
+      img:"https://previews.123rf.com/images/subbotina/subbotina1601/subbotina160100032/50758762-fashion-model-girl-isolated-over-white-background.jpg",
+      title:"FESTIVE SALE",
+}];
